d8f89aa7a95dd1c1d1cb8f9a0133be0ab7c84779
noelo <noel.oconnor@gmail.com>
2022-09-14 15:34:58 +0100

    mvp working

--- src/main/java/com/example/noc/DataStreamJob.java
+++ src/main/java/com/example/noc/DataStreamJob.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.noc;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
@@ -23,53 +5,69 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.pulsar.common.config.PulsarOptions;
 import org.apache.flink.connector.pulsar.source.PulsarSource;
+import org.apache.flink.connector.pulsar.source.PulsarSourceOptions;
 import org.apache.flink.connector.pulsar.source.enumerator.cursor.StartCursor;
 import org.apache.flink.connector.pulsar.source.reader.deserializer.PulsarDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.pulsar.client.api.SubscriptionType;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+
 public class DataStreamJob {
 
-	public static void main(String[] args) throws Exception {
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting....getting env");
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		PulsarSource<String> fixedSource = PulsarSource.builder()
-				.setServiceUrl("pulsar+ssl://sslproxy-route-pulsar.apps.ocp.sno.themadgrape.com:443")
-				.setAdminUrl("https://sslproxy-https-route-pulsar.apps.ocp.sno.themadgrape.com")
-				.setStartCursor(StartCursor.earliest())
-				.setTopics("persistent://public/default/TestDataTopic")
-				.setDeserializationSchema(PulsarDeserializationSchema.flinkSchema(new SimpleStringSchema()))
-				.setSubscriptionName("DataStreamJob")
-				.setSubscriptionType(SubscriptionType.Exclusive)
-				.setConfig(PulsarOptions.PULSAR_TLS_HOSTNAME_VERIFICATION_ENABLE,Boolean.FALSE)
-				.setConfig(PulsarOptions.PULSAR_TLS_TRUST_CERTS_FILE_PATH, "/home/noelo/dev/noc-pulsar-client/client/certs/pulsar-proxy.pem")
-				.setConfig(PulsarOptions.PULSAR_TLS_ALLOW_INSECURE_CONNECTION, Boolean.TRUE)
-				.build();
+        PulsarSource<String> fixedSource = PulsarSource.builder()
+                .setServiceUrl("pulsar+ssl://sslproxy-route-pulsar.apps.ocp.sno.themadgrape.com:443")
+                .setAdminUrl("https://sslproxy-https-route-pulsar.apps.ocp.sno.themadgrape.com")
+                .setStartCursor(StartCursor.earliest())
+                .setTopics("TestDataTopic")
+                .setDeserializationSchema(PulsarDeserializationSchema.flinkSchema(new SimpleStringSchema()))
+                .setSubscriptionName("DataStreamJob")
+                .setSubscriptionType(SubscriptionType.Shared)
+                .setConfig(PulsarOptions.PULSAR_TLS_HOSTNAME_VERIFICATION_ENABLE, Boolean.FALSE)
+                .setConfig(PulsarOptions.PULSAR_TLS_TRUST_CERTS_FILE_PATH, "/home/noelo/dev/noc-pulsar-client/client/certs/pulsar-proxy.pem")
+                .setConfig(PulsarOptions.PULSAR_TLS_ALLOW_INSECURE_CONNECTION, Boolean.TRUE)
+                .setConfig(PulsarSourceOptions.PULSAR_ENABLE_AUTO_ACKNOWLEDGE_MESSAGE, Boolean.TRUE)
+                .build();
 
-		DataStream<String> fixedStream = env.fromSource(fixedSource, WatermarkStrategy.noWatermarks(), "Pulsar Source");
+        DataStream<String> fixedStream = env.fromSource(fixedSource, WatermarkStrategy.noWatermarks(), "Pulsar Source")
+                .map(new MapFunction<String, String>() {
+                    @Override
+                    public String map(String value) throws Exception {
+                        return value.toUpperCase()+String.valueOf(System.currentTimeMillis());
+                    }
+                });
 
-		fixedStream.map(new MapFunction<String, String>() {
-			@Override
-			public String map(String value) throws Exception {
-				System.out.println(value);
-				return value;
-			}
-		});
+        fixedStream.addSink(new PrintSinkFunction<>("outputSink", Boolean.TRUE));
+        env.execute("DataStreamJob");
 
-		// Execute program, beginning computation.
-		env.execute("DataStreamJob");
-	}
+    }
 }
+
+
+//	PulsarSink<String> fixedDest = PulsarSink.builder()
+//				.setServiceUrl("pulsar+ssl://sslproxy-route-pulsar.apps.ocp.sno.themadgrape.com:443")
+//				.setAdminUrl("https://sslproxy-https-route-pulsar.apps.ocp.sno.themadgrape.com")
+//				.setTopics("persistent://public/default/TestDataTopicSink")
+//				.setSerializationSchema(PulsarSerializationSchema.flinkSchema(new SimpleStringSchema()))
+//				.setConfig(PulsarOptions.PULSAR_TLS_HOSTNAME_VERIFICATION_ENABLE,Boolean.FALSE)
+//				.setConfig(PulsarOptions.PULSAR_TLS_TRUST_CERTS_FILE_PATH, "/home/noelo/dev/noc-pulsar-client/client/certs/pulsar-proxy.pem")
+//				.setConfig(PulsarOptions.PULSAR_TLS_ALLOW_INSECURE_CONNECTION, Boolean.TRUE)
+//				.build();
+
+//    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//    PulsarSource<String> source = PulsarSource.builder()
+//            .setServiceUrl("pulsar://47.102.192.43:6650")
+//            .setAdminUrl("http://47.102.192.43:8888")
+//            .setStartCursor(StartCursor.earliest())
+//            .setTopics("dev-message")
+//            .setDeserializationSchema(PulsarDeserializationSchema.flinkSchema(new SimpleStringSchema()))
+//            .setSubscriptionName("flink-subscription")
+//            .setSubscriptionType(SubscriptionType.Exclusive).build();
+//    DataStreamSource<String> dataStreamSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Pulsar Source Work");
+//    dataStreamSource.print();
+//    try{env.execute("pulsar");}catch(Exception e){e.printStackTrace();}
