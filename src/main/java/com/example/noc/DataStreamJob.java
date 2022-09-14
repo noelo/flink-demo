09af437029a0e2ea761a4e3f9d3cf5e6d1d688a5
noelo <noel.oconnor@gmail.com>
2022-09-14 18:06:23 +0100

    map using java lambda code tidy up now with file

--- src/main/java/com/example/noc/DataStreamJob.java
+++ src/main/java/com/example/noc/DataStreamJob.java
@@ -46,40 +46,10 @@ public class DataStreamJob {
                 .build();
 
         DataStream<String> fixedStream = env.fromSource(fixedSource, WatermarkStrategy.noWatermarks(), "Pulsar Source")
-                .map(new MapFunction<String, String>() {
-                    @Override
-                    public String map(String value) throws Exception {
-                        return value.toUpperCase()+String.valueOf(System.currentTimeMillis());
-                    }
-                });
+                .map(k->{return k.toUpperCase()+String.valueOf(System.currentTimeMillis());});
 
         fixedStream.addSink(new PrintSinkFunction<>("outputSink", Boolean.TRUE));
         fixedStream.sinkTo(fixedDest);
         env.execute("DataStreamJob");
-
     }
 }
-
-
-//	PulsarSink<String> fixedDest = PulsarSink.builder()
-//				.setServiceUrl("pulsar+ssl://sslproxy-route-pulsar.apps.ocp.sno.themadgrape.com:443")
-//				.setAdminUrl("https://sslproxy-https-route-pulsar.apps.ocp.sno.themadgrape.com")
-//				.setTopics("persistent://public/default/TestDataTopicSink")
-//				.setSerializationSchema(PulsarSerializationSchema.flinkSchema(new SimpleStringSchema()))
-//				.setConfig(PulsarOptions.PULSAR_TLS_HOSTNAME_VERIFICATION_ENABLE,Boolean.FALSE)
-//				.setConfig(PulsarOptions.PULSAR_TLS_TRUST_CERTS_FILE_PATH, "/home/noelo/dev/noc-pulsar-client/client/certs/pulsar-proxy.pem")
-//				.setConfig(PulsarOptions.PULSAR_TLS_ALLOW_INSECURE_CONNECTION, Boolean.TRUE)
-//				.build();
-
-//    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//    PulsarSource<String> source = PulsarSource.builder()
-//            .setServiceUrl("pulsar://47.102.192.43:6650")
-//            .setAdminUrl("http://47.102.192.43:8888")
-//            .setStartCursor(StartCursor.earliest())
-//            .setTopics("dev-message")
-//            .setDeserializationSchema(PulsarDeserializationSchema.flinkSchema(new SimpleStringSchema()))
-//            .setSubscriptionName("flink-subscription")
-//            .setSubscriptionType(SubscriptionType.Exclusive).build();
-//    DataStreamSource<String> dataStreamSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Pulsar Source Work");
-//    dataStreamSource.print();
-//    try{env.execute("pulsar");}catch(Exception e){e.printStackTrace();}
